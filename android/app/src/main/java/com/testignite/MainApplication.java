5797de76ddb9d4cb6812fd1bf5065209be72c7e2
prakosa <prakosa.ananta@gmail.com>
2017-12-03 21:54:37 +0700

    first commit

--- android/app/src/main/java/com/testignite/MainApplication.java
+++ android/app/src/main/java/com/testignite/MainApplication.java
@@ -3,6 +3,8 @@ package com.testignite;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ReactNativeI18n(),
+            new VectorIconsPackage()
       );
     }
 
